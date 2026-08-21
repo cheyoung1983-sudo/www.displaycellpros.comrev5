7a90cd4c3ad9141e913bbe9b4b92397c9850316b
cheyoung1983-sudo <cheyoung1983@gmail.com>
2026-08-20 18:48:32 -0700

    latest

--- next.config.js
+++ next.config.js
@@ -22,6 +22,9 @@ const securityHeaders = [
 
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    reactProductionProfiling: true,
+  },
   async headers() {
     return [
       {
